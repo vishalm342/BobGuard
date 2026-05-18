8b32245e678b9fff93c2f5bf9836c8f659deadfc
vishalm342 <vishalm8656@gmail.com>
2026-05-18 10:44:03 +0530

    fix: cors credentials and frontend fetch cookies

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app.include_router(bob_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
